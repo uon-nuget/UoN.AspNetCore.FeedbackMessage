dd4e827d79dd9cbd888ce25b8c77295f085cc2a7
Jonathan Couldridge <jcouldridge@outlook.com>
2017-11-02 14:51:03 +0000

    renamed sln, rejigged controller to not use FeedbackMessageModel directly

--- UoN.AspNetCore.FeedbackMessage/FeedbackMessageAjaxController.cs
+++ UoN.AspNetCore.FeedbackMessage/FeedbackMessageAjaxController.cs
@@ -1,13 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace UoN.AspNetCore.FeedbackMessage
 {
     public class FeedbackMessageAjaxController : Controller
     {
-        public IActionResult Index(FeedbackMessageModel model)
+        public IActionResult Index(string message, string type)
         {
-            this.SetFeedbackMessage(model.Message, model.Type);
+            AlertTypes t;
+            try
+            {
+                t = (AlertTypes) Enum.Parse(typeof(AlertTypes), type, true);
+            }
+            //These are exceptions that might throw if type can't be parsed as an AlertTypes value
+            catch (Exception e) when (
+                e is ArgumentException ||
+                e is OverflowException)
+            {
+                t = AlertTypes.Secondary; //default
+            }
 
+            this.SetFeedbackMessage(message, t);
             return PartialView("UoNFeedbackMessage");
         }
     }
