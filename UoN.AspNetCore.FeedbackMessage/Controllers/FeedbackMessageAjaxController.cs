f84ac0c36bb220b90dc6389440e03c05d3a2d5fb
Jon Couldridge <jcouldridge@outlook.com>
2018-11-29 11:29:39 +0000

    TagHelper now renders the ViewComponent

--- UoN.AspNetCore.FeedbackMessage/Controllers/FeedbackMessageAjaxController.cs
+++ UoN.AspNetCore.FeedbackMessage/Controllers/FeedbackMessageAjaxController.cs
@@ -6,12 +6,10 @@ namespace UoN.AspNetCore.FeedbackMessage.Controllers
     public class FeedbackMessageAjaxController : Controller
     {
         public IActionResult Index(string message, string type = null)
-        {
-            return ViewComponent("UonFeedbackMessage", new FeedbackMessageModel
+            => ViewComponent("UonFeedbackMessage", new FeedbackMessageModel
             {
                 Message = message,
                 Type = type
             });
-        }
     }
 }
