40bd9613517ae230fa0f09eb6eaddc5ea17868c5
Dan Giddins <daniel.giddins@nottingham.ac.uk>
2019-01-09 16:22:47 +0000

    allowed for multiple feedback messages

--- UoN.AspNetCore.FeedbackMessage/Extensions.cs
+++ UoN.AspNetCore.FeedbackMessage/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using UoN.AspNetCore.FeedbackMessage.Models;
 
 namespace UoN.AspNetCore.FeedbackMessage
@@ -18,55 +19,20 @@ namespace UoN.AspNetCore.FeedbackMessage
         /// <summary>
         /// Controller extension method, so any controller action can do `this.SetFeedBackMessage()`
         /// </summary>
-        /// <param name="model">A model containing all the properties of the Feedback Message to set</param>
-        public static void SetFeedbackMessage(this Controller controller, FeedbackMessageModel model)
+        /// <param name="model">A list of type model containing all the properties of the Feedback Message to set</param>
+        public static void SetFeedbackMessages(
+            this Controller controller,
+            IList<FeedbackMessageModel> model)
             => controller.TempData[Key] = JsonConvert.SerializeObject(model);
-
-        /// <summary>
-        /// Controller extension method, so any controller action can do `this.SetFeedBackMessage()`
-        /// </summary>
-        /// <param name="message">The HTML content of the message</param>
-        /// <param name="type">The type of alert</param>
-        public static void SetFeedbackMessage(this Controller controller, string message, string type)
-            => controller.SetFeedbackMessage(new FeedbackMessageModel { Message = message, Type = type });
-
-        /// <summary>
-        /// Controller extension method, so any controller action can do `this.SetFeedBackMessage()`
-        /// </summary>
-        /// <param name="message">The HTML content of the message</param>
-        public static void SetFeedbackMessage(this Controller controller, string message)
-            => controller.SetFeedbackMessage(new FeedbackMessageModel { Message = message });
-
-        /// <summary>
-        /// Controller extension method, so any controller action can do `this.SetFeedBackMessage()`
-        /// </summary>
-        /// <param name="message">The HTML content of the message</param>
-        /// <param name="dismissable">Should the alert be able to be dismissed?</param>
-        public static void SetFeedbackMessage(this Controller controller, string message, bool dismissable)
-            => controller.SetFeedbackMessage(new FeedbackMessageModel { Message = message, Dismissable = dismissable });
-
-        /// <summary>
-        /// Controller extension method, so any controller action can do `this.SetFeedBackMessage()`
-        /// </summary>
-        /// <param name="message">The HTML content of the message</param>
-        /// <param name="type">The type of alert</param>
-        /// <param name="dismissable">Should the alert be able to be dismissed?</param>
-        public static void SetFeedbackMessage(this Controller controller, string message, string type, bool dismissable)
-            => controller.SetFeedbackMessage(new FeedbackMessageModel
-            {
-                Message = message,
-                Type = type,
-                Dismissable = dismissable
-            });
 
         /// <summary>
         /// TempData extension, that handles casting, Key access for you
         /// </summary>
         /// <returns>The FeedbackMessageModel stored by `SetFeedbackMessage()`</returns>
-        public static FeedbackMessageModel GetFeedbackMessage(this ITempDataDictionary tempData)
+        public static List<FeedbackMessageModel> GetFeedbackMessages(this ITempDataDictionary tempData)
         {
             tempData.TryGetValue(Key, out var o);
-            return o == null ? null : JsonConvert.DeserializeObject<FeedbackMessageModel>((string)o);
+            return o == null ? null : JsonConvert.DeserializeObject<List<FeedbackMessageModel>>((string)o);
         }
     }
 }
